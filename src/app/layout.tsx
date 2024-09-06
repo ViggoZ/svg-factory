a54a05a63065d2fcc7cc728c53401f21240df5ad
ViggoZ <272910299@qq.com>
2024-09-06 11:04:59 +0800

    GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,6 +64,33 @@ export default function RootLayout({
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-TileImage" content="/favicon/mstile-144x144.png" />
         <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6MLV747W83"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6MLV747W83');
+            `,
+          }}
+        />
+
+        {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "nyrvz8ez4r");
+            `,
+          }}
+        />
       </head>
       <body className={`flex flex-col min-h-screen bg-[#E2E1CF] text-neutral-800`}>
         <HeaderNav />
